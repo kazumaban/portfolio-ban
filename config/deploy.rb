338214c918ab56b4671dcc3015452243cbc31f3b
kazumaban <1310350007t@gmail.com>
2020-12-23 19:46:45 +0900

    自動デプロイ01

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'portfolio-ban'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:bankazuma/portfolio-ban.git'
+set :repo_url,  'git@github.com:kazumaban/portfolio-ban.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
